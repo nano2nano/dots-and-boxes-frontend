57d841277522100ddd7ef07cc6da24990c43183d
nano <76194982+nano2nano@users.noreply.github.com>
2020-12-29 17:56:14 +0900

    init

---
+++ src/App.vue
@@ -0,0 +1,166 @@
+<template>
+    <div id="app">
+        <Loading
+            :active.sync="waiting"
+            :is-full-screen="true"
+            :can-cancel="false"
+        ></Loading>
+        <div class="screen">
+            <div>Your color is Blue</div>
+            <div v-if="!this.done">
+                Turn : {{ 59 - available_actions.length }}
+            </div>
+            <div v-else>
+                <div v-if="this.score[0] > this.score[1]">Winner is Blue</div>
+                <div v-else-if="this.score[0] < this.score[1]">
+                    Winner is Red
+                </div>
+                <div v-else>引き分けです</div>
+            </div>
+
+            <table align="center">
+                <tr v-for="x in 9" :key="x">
+                    <td
+                        v-for="y in 13"
+                        :key="10 * y + x"
+                        @click="set_bar(x, y)"
+                    >
+                        <Item
+                            :type="get_type(x, y)"
+                            :color="board[x - 1][y - 1]"
+                        ></Item>
+                    </td>
+                </tr>
+            </table>
+        </div>
+    </div>
+</template>
+
+<script>
+import Item from "@/components/Item.vue";
+import axios from "axios";
+import Loading from "vue-loading-overlay";
+import "vue-loading-overlay/dist/vue-loading.css";
+
+export default {
+    name: "App",
+    components: {
+        Item,
+        Loading,
+    },
+    data: function () {
+        return {
+            board: [],
+            turn: -1,
+            score: [0, 0],
+            available_actions: [...Array(58).keys()],
+            done: false,
+            turn_cnt: 1,
+            waiting: false,
+        };
+    },
+    methods: {
+        init_board() {
+            var new_board = [];
+            for (var x = 0; x < 9; x++) {
+                var boardLine = [];
+                for (var y = 0; y < 13; y++) {
+                    boardLine.push(0);
+                }
+                new_board.push(boardLine);
+            }
+            this.board = new_board;
+        },
+        get_type(x, y) {
+            if (this.is_dot(x, y)) {
+                return 0;
+            } else if (this.is_box(x, y)) {
+                return 1;
+            } else if (this.is_bar(x, y)) {
+                if (x % 2 == 0) {
+                    return 2;
+                } else {
+                    return 3;
+                }
+            } else {
+                throw "Unexpected type";
+            }
+        },
+        set_bar(x, y) {
+            if (
+                this.waiting ||
+                !this.is_bar(x, y) ||
+                this.board[x - 1][y - 1] != 0
+            ) {
+                return;
+            }
+            const url = "https://dqn-dots-and-boxes.herokuapp.com/step";
+            this.waiting = true;
+            axios
+                .post(url, {
+                    board: this.board,
+                    score: this.score,
+                    turn: this.turn,
+                    available_actions: this.available_actions,
+                    action: [x - 1, y - 1],
+                })
+                .then((response) => {
+                    this.board = JSON.parse(response.data["board"]);
+                    this.turn = JSON.parse(response.data["turn"]);
+                    this.available_actions = JSON.parse(
+                        response.data["available_actions"]
+                    );
+                    this.score = JSON.parse(response.data["score"]);
+                    this.done = JSON.parse(response.data["done"]);
+                    this.waiting = false;
+                });
+        },
+        is_bar(x, y) {
+            if (x % 2 != y % 2) {
+                return true;
+            } else {
+                return false;
+            }
+        },
+        is_dot(x, y) {
+            if (x % 2 == 1 && y % 2 == 1) {
+                return true;
+            } else {
+                return false;
+            }
+        },
+        is_box(x, y) {
+            if (x % 2 == 0 && y % 2 == 0) {
+                return true;
+            } else {
+                return false;
+            }
+        },
+        copy_board() {
+            return JSON.parse(JSON.stringify(this.board));
+        },
+        wake_server() {
+            this.waiting = true;
+            const url = "https://dqn-dots-and-boxes.herokuapp.com/";
+            axios.get(url).then(() => {
+                this.waiting = false;
+            });
+        },
+    },
+    created() {
+        this.init_board();
+        this.wake_server();
+    },
+};
+</script>
+
+<style>
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+</style>
---
+++ src/components/Item.vue
@@ -0,0 +1,81 @@
+<template>
+    <img :src="content" width="50" height="50" />
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            black_box: require("@/assets/black_box.png"),
+            black_horizontal: require("@/assets/black_horizontal.png"),
+            black_vertical: require("@/assets/black_vertical.png"),
+            blue_box: require("@/assets/blue_box.png"),
+            blue_horizontal: require("@/assets/blue_horizontal.png"),
+            blue_vertical: require("@/assets/blue_vertical.png"),
+            red_box: require("@/assets/red_box.png"),
+            red_horizontal: require("@/assets/red_horizontal.png"),
+            red_vertical: require("@/assets/red_vertical.png"),
+            dot: require("@/assets/dot.png"),
+        };
+    },
+    props: {
+        type: Number,
+        color: Number,
+    },
+    computed: {
+        content() {
+            var path;
+            switch (this.type) {
+                case 0:
+                    path = this.dot;
+                    break;
+                case 1:
+                    switch (this.color) {
+                        case -1:
+                            path = this.blue_box;
+                            break;
+                        case 0:
+                            path = this.black_box;
+                            break;
+                        case 1:
+                            path = this.red_box;
+                            break;
+                    }
+                    break;
+                case 2:
+                    switch (this.color) {
+                        case -1:
+                            path = this.blue_vertical;
+                            break;
+                        case 0:
+                            path = this.black_vertical;
+                            break;
+                        case 1:
+                            path = this.red_vertical;
+                            break;
+                    }
+                    break;
+                case 3:
+                    switch (this.color) {
+                        case -1:
+                            path = this.blue_horizontal;
+                            break;
+                        case 0:
+                            path = this.black_horizontal;
+                            break;
+                        case 1:
+                            path = this.red_horizontal;
+                            break;
+                    }
+                    break;
+                default:
+                    throw "Unexpected type";
+            }
+            return path;
+        },
+    },
+};
+</script>
+
+<style scoped>
+</style>
